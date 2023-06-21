7e195e113c278295ab6cc662b67d2e9821c3ee86
Cameron Oberlies <cameron.oberlies@outlook.com>
2023-06-20 22:03:48 -0500

    fix req.body bug, import sequelize in server.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,7 +34,7 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  Category.update(req, body, {
+  Category.update(req.body, {
     where: {
       id: req.params.id,
     },
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -46,7 +46,7 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
-  Tag.update(req, body, {
+  Tag.update(req.body, {
     where: {
       id: req.params.id,
     },
